643cdbe008e640d9328979354525dc65e85241be
unknown <645841363@qq.com>
2020-04-17 21:10:04 +0800

    p02

---
+++ p02/src/main/java/webadv/s99201105/p02/App.java
@@ -0,0 +1,29 @@
+package webadv.s99201105.p02;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入账号：");
+        String user = scan.nextLine();
+        System.out.println("请输入密码：");
+        String pwd = scan.nextLine();
+        if (user.equals( "17201119") && pwd.equals( "123456"))
+        {
+             System.out.println("登录成功!");
+        }else{
+             System.out.println("登录失败!");
+        }
+        if (args.length < 1) {
+            System.err.println("Please provide an input!");
+            System.exit(0);
+        }
+        System.out.println(sha256hex(args[0]));
+    }
+    public static String sha256hex(String input) {
+        return DigestUtils.sha256Hex(input);
+    }
+}
+
